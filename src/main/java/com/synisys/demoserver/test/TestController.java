8b830e7dea00a630972a017d2b02330c3bf330fc
artush.ghandilyan <artush.ghandilyan@synisys.com>
2017-11-09 02:56:13 +0400

    test controller

--- src/main/java/com/synisys/demoserver/test/TestController.java
+++ src/main/java/com/synisys/demoserver/test/TestController.java
@@ -3,8 +3,6 @@ package com.synisys.demoserver.test;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.synisys.demoserver.sketch.service.DemoService;
 
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * @author Artush on 11/3/2017.
@@ -29,29 +26,13 @@ public class TestController {
 
 	@GetMapping(value = "/stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
 	public Flux<TestModel> stream() {
-		return Flux.range(1, 1000)
-				.map(integer -> {
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					return integer;
-				})
+		return Flux.range(1, 1000)/*.delayElements(Duration.ofMillis(10))*/
 				.map(TestModel::new);
 	}
 
 	@GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Flux<TestModel> list() {
-		return Flux.range(1, 1000)
-				.map(integer -> {
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					return integer;
-				})
+		return Flux.range(1, 1000)/*.delayElements(Duration.ofMillis(300))*/
 				.map(TestModel::new);
 	}
 
@@ -59,8 +40,8 @@ public class TestController {
 	public List<TestModel> handle()
 			throws InterruptedException {
 		List<TestModel> result = new ArrayList<>();
-		for (int i = 0; i < 1000; i++) {
-			Thread.sleep(10);
+		for (int i = 0; i < 10; i++) {
+			Thread.sleep(300);
 			result.add(new TestModel(i));
 		}
 		return result;
